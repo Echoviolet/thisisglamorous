cf7d8b1debda93ca175be3cb115c2150525f3e52
Echo <echo@server.melody.private>
2016-11-01 17:43:03 +0800

    finish index.html

--- javascipt/thisisglamorous.js
+++ javascipt/thisisglamorous.js
@@ -4,64 +4,122 @@ $(document).ready(function(){
     //Design
     $('.design').mouseenter(function(){
         $('.decor').show();
+        $('.inspiration').hide();
+        $('.travel').hide();
+        $('.fashion').hide();
+        $('.culture').hide();
+        $('.food').hide();
+        $('.to').hide();
+        $('.shop').hide();
+
     });
-    $('.design').mouseleave(function(){
+    $('.decor').mouseleave(function(){
         $('.decor').hide();
     });
 
     //Inspiration
     $('.inspire').mouseenter(function(){
+        $('.decor').hide();
         $('.inspiration').show();
+        $('.travel').hide();
+        $('.fashion').hide();
+        $('.culture').hide();
+        $('.food').hide();
+        $('.to').hide();
+        $('.shop').hide();
     });
-    $('.inspire').mouseleave(function(){
+    $('.inspiration').mouseleave(function(){
         $('.inspiration').hide();
     });
 
     //Travel
     $('.traveller').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
         $('.travel').show();
+        $('.fashion').hide();
+        $('.culture').hide();
+        $('.food').hide();
+        $('.to').hide();
+        $('.shop').hide();
     });
-    $('.traveller').mouseleave(function(){
+    $('.travel').mouseleave(function(){
         $('.travel').hide();
     });
 
     //Fashion
     $('.vogue').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
+        $('.travel').hide();
         $('.fashion').show();
+        $('.culture').hide();
+        $('.food').hide();
+        $('.to').hide();
+        $('.shop').hide();
     });
-    $('.vogue').mouseleave(function(){
+    $('.fashion').mouseleave(function(){
         $('.fashion').hide();
     });
 
     //Arts
     $('.art').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
+        $('.travel').hide();
+        $('.fashion').hide();
         $('.culture').show();
+        $('.food').hide();
+        $('.to').hide();
+        $('.shop').hide();
     });
-    $('.art').mouseleave(function(){
+    $('.culture').mouseleave(function(){
         $('.culture').hide();
     });
 
     //Food
     $('.goods').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
+        $('.travel').hide();
+        $('.fashion').hide();
+        $('.culture').hide();
         $('.food').show();
+        $('.to').hide();
+        $('.shop').hide();
+
     });
-    $('.goods').mouseleave(function(){
+    $('.food').mouseleave(function(){
         $('.food').hide();
     });
 
     //How-to
     $('.how').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
+        $('.travel').hide();
+        $('.fashion').hide();
+        $('.culture').hide();
+        $('.food').hide();
         $('.to').show();
+        $('.shop').hide();
     });
-    $('.how').mouseleave(function(){
+    $('.to').mouseleave(function(){
         $('.to').hide();
     });
 
     //Shop
     $('.shopping').mouseenter(function(){
+        $('.decor').hide();
+        $('.inspiration').hide();
+        $('.travel').hide();
+        $('.fashion').hide();
+        $('.culture').hide();
+        $('.food').hide();
+        $('.to').hide();
         $('.shop').show();
     });
-    $('.shopping').mouseleave(function(){
+    $('.shop').mouseleave(function(){
         $('.shop').hide();
     });
 
@@ -75,6 +133,7 @@ $(document).ready(function(){
            }
        });
 
+    //
     $('.left').mouseenter(function(){
         $('.mask').fadeIn(500);
     });
@@ -82,6 +141,7 @@ $(document).ready(function(){
         $('.mask').fadeOut(500);
     });
 
+    //
     $('.upper').mouseenter(function(){
         $('.mask1').fadeIn(500);
     });
@@ -89,6 +149,7 @@ $(document).ready(function(){
         $('.mask1').fadeOut(500);
     });
 
+    //
     $('.showToggle').mouseenter(function(){
         $('.mask3').fadeIn(500);
     });
@@ -96,6 +157,7 @@ $(document).ready(function(){
         $('.mask3').fadeOut(500);
     });
 
+    //
     $('.showToggle1').mouseenter(function(){
         $('.mask4').fadeIn(500);
     });
@@ -103,12 +165,15 @@ $(document).ready(function(){
         $('.mask4').fadeOut(500);
     });
 
+    //
     $('.cover').mouseenter(function(){
         $('.cover').fadeOut('slow');
     });
     $('.black-image').mouseleave(function(){
         $('.cover').fadeIn('slow');
     });
+
+    //
     $('.cover1').mouseenter(function(){
         $('.cover1').fadeOut('slow');
     });
@@ -116,8 +181,16 @@ $(document).ready(function(){
         $('.cover1').fadeIn('slow');
     });
 
+//goTop
+    $('.goTop a').click(function(){
+        $('html,body').animate({scrollTop:'0px'},500);
+    });
 
-
-
-
+    //
+    $('.mask-flower').mouseenter(function(){
+        $('.mask-flower').fadeOut(1000);
+    });
+    $('.middle-picture').mouseleave(function(){
+        $('.mask-flower').fadeIn(1000);
+    });
 });
