a187ac2ed4b86bec1f0baadce6dfa394b11031a0
Echo <echo@server.melody.private>
2016-11-02 18:13:44 +0800

    shop.html

---
+++ javascipt/shop.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    'use strict';
+    $(window).bind('scroll',function(){
+        var tmp = $(window).scrollTop();
+        if (tmp >100){
+            $('.logo-picture').animate({height:'55px'});
+        }else
+        {
+            $('.logo-picture').animate({height:'125px'});
+        }
+    });
+});
+
+
