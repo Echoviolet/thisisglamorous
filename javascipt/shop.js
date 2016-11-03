ca233444d9ef0460e6e350acf50ee7d1b9f1b7ff
Echo <echo@server.melody.private>
2016-11-03 09:30:51 +0800

    tmp

--- javascipt/shop.js
+++ javascipt/shop.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     'use strict';
     $(window).bind('scroll',function(){
         var tmp = $(window).scrollTop();
-        if (tmp >100){
-            $('.logo-picture').animate({height:'55px'});
-        }else
-        {
-            $('.logo-picture').animate({height:'125px'});
-        }
+        //if (tmp >100){
+        //    $('.logo-picture').animate({height:'55px'});
+        //}else
+        //{
+        //    $('.logo-picture').animate({height:'125px'});
+        //}
     });
 });
 
