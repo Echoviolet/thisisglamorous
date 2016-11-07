3f441310ea98794359d66e419e6f320bef7ce7b9
Echo <echo@server.melody.private>
2016-11-07 16:38:09 +0800

    tmp

--- javascipt/fashion.js
+++ javascipt/fashion.js
@@ -1,3 +1,18 @@
-/**
- * Created by echo on 16/10/27.
- */
+(function(){
+    'use strict';
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+
+        // 如果需要滚动条
+        //scrollbar: '.swiper-scrollbar',
+    })
+})();
