31b6e276b29b90948648d6da19eaf955573feecc
Echo <echo@server.melody.private>
2016-11-08 16:43:56 +0800

    finish pagination

--- javascipt/fashion.js
+++ javascipt/fashion.js
@@ -27,4 +27,27 @@ $(document).ready(function(){
     $('.swiper-button-next').mouseleave(function(){
         $('.swiper-button-next').css('opacity','0.3');
     });
+
+
+    //分页插件
+
+    $(function(){
+        var initPagination = function() {
+            var num_entries = $('.allPicture-hide .main-container').length;
+            // 创建分页
+            $("#Pagination").pagination(num_entries, {
+                num_edge_entries: 2, //边缘页数
+                num_display_entries: 3, //主体页数
+                callback: pageselectCallback,
+                items_per_page:1 //每页显示1项
+            });
+        }();
+
+        function pageselectCallback(page_index, jq){
+            var new_content = $(".allPicture-hide .main-container:eq("+page_index+")").clone();
+            $(".allPicture").empty().append(new_content); //装载对应分页的内容
+            return false;
+        }
+    });
+
 });
