3089a792ef92cdc2c32b62d4290107e2e6beb4d2
Echo <echo@server.melody.private>
2016-11-11 18:31:24 +0800

    tmp

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 7000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname + '/views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
